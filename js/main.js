15108196fc667b07b55545439f6627de6efdc4d2
DiabloDraco <mirmuhsinmirahmatov@gmail.com>
2022-08-23 19:29:58 +0500

    addd

--- js/main.js
+++ js/main.js
@@ -27,12 +27,16 @@ function renderRow(array) {
         let template = elRowTemplate.cloneNode(true)
 
         template.querySelector(".name").textContent = array[i].title
+        template.querySelector(".name").classList.add(`name${array[i]._id}`)
         template.querySelector(".mail").textContent = array[i].body
+        template.querySelector(".mail").classList.add(`body${array[i]._id}`)
         template.querySelector(".id").textContent = array[i]._id
         template.querySelector(".num").textContent = [i]
         template.querySelector(".btn-remove").dataset.removeId = array[i]._id
+        template.querySelector(".btn-edit").dataset.editId = array[i]._id
         template.querySelector(".btn-remove").classList.add(`rem${array[i]._id}`)
-
+        template.querySelector(".btn-edit").classList.add(`edit${array[i]._id}`)
+        
         fragment.appendChild(template)
     }
     elWrapper.appendChild(fragment)
@@ -46,12 +50,52 @@ elLogOut.addEventListener("click" , function () {
 })
 
 elWrapper.addEventListener("click" , function (evt) {
-    let removeObject = evt.target.dataset.removeId
-    if (removeObject) {
-        let removeEl = document.querySelector(`.rem${removeObject}`)
+    let Object = evt.target.dataset
+    if (Object.removeId) {
+        let removeEl = document.querySelector(`.rem${Object.removeId}`)
 
         let element = removeEl.closest(".rowcha")
-
+        fetch(`https://fast-ravine-16741.herokuapp.com/api/posts/${Object.removeId}` , {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": token
+            }
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
         element.remove()
+    }
+    if (Object.editId) {
+        let elName = document.querySelector(`.name${Object.editId}`)
+        let elBody = document.querySelector(`.body${Object.editId}`)
+
+        console.log(elName);
+
+        let modalTitle = document.querySelector(".edit-title")
+        let modalBody = document.querySelector(".edit-body")
+
+        let edit = document.querySelector(".edit-button")
+        edit.addEventListener("click" , function () {
+            let name = modalTitle.value
+            let body = modalBody.value
+            elName.textContent = modalTitle.value
+            elBody.textContent = modalBody.value
+            fetch(`https://fast-ravine-16741.herokuapp.com/api/posts/${Object.editId}` , {
+                method : "PUT", 
+                headers: {
+                    Authorization : token , 
+                    "Content-Type": "application/json"
+                },
+                body : JSON.stringify(
+                    {
+                        "title": name,
+                        "body":body
+                    }
+                )
+            })
+            name.value = null
+            body.value = null
+        })
     }
 })
